f68e69c508c04c974268d9a35df22f3f4db2098f
HWChia <66512878+xmas21@users.noreply.github.com>
2020-08-15 21:03:21 +0800

    選單動畫 - 實作畫面切換與ao的操作

--- 3DRPG/Assets/Scripts/MenuManager.cs
+++ 3DRPG/Assets/Scripts/MenuManager.cs
@@ -1,7 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement; // Scenemanager 需要此API
+using System.Collections; // 協程需要此API
 
+#region 欄位
 public class MenuManager : MonoBehaviour
 {
+    [Header("載入畫面")]
+    public GameObject panelLoading;  // 整組的就用 GameObject
+    [Header("進度")]
+    public Text textLoading;         // 文字訊息用 Text
+    [Header("進度條")]
+    public Image imgLoading;         // 圖像使用 Image
+    [Header("載入畫面")]
+    public string nameScene = "遊戲場景";
+
+    #endregion
+
+    /// <summary>
+    /// 開始遊戲
+    /// </summary>
+    public void StartGame()
+    {
+        StartCoroutine(Loading());
+    }
+
     /// <summary>
     /// 離開遊戲
     /// </summary>
@@ -9,4 +32,21 @@ public class MenuManager : MonoBehaviour
     {
         Application.Quit();
     }
+
+
+    private IEnumerator Loading()
+    {
+        panelLoading.SetActive(true);                               // 顯示載入畫面
+        AsyncOperation ao = SceneManager.LoadSceneAsync(nameScene); // 同步載入場景(場景名稱)
+        ao.allowSceneActivation = false;                            // 不要自動載入場景
+
+        // 當 尚未載入場景
+        while (!ao.isDone)
+        {
+            textLoading.text = ao.progress * 100 + "%"; // 更新文字
+            imgLoading.fillAmount = ao.progress;        // 更新進度條
+            yield return null;                          // 延遲一個影格時間
+        }
+    }
+
 }
