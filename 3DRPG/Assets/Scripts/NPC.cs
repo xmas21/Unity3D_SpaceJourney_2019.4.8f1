2ebd004cb2ca0e02adeb208972fb94673ee45026
HWChia <66512878+xmas21@users.noreply.github.com>
2020-08-29 16:55:35 +0800

    NPC打字音效與動畫

--- 3DRPG/Assets/Scripts/NPC.cs
+++ 3DRPG/Assets/Scripts/NPC.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class NPC : MonoBehaviour
 {
@@ -11,12 +12,41 @@ public class NPC : MonoBehaviour
     public Text textName;
     [Header("說話內容")]
     public Text textContent;
+    [Header("打字速度"), Range(0.1f, 1)]
+    public float printSpeed = 0.2f ;
+    [Header("打字音效")]
+    public AudioClip soundPrint;
 
+    private AudioSource aud;
+
+    /// <summary>
+    /// 對話系統
+    /// </summary>
     public void dialog()
     {
         paneldialog.SetActive(true);
         textName.text = name;
-        textContent.text = data.dialogs[0];
+        StartCoroutine(Print());
+    }
+
+    /// <summary>
+    /// 打字效果
+    /// </summary>
+    private IEnumerator Print()
+    {
+        string dialog = data.dialogs[0];                  // 對著 = NPC 資料.對話第一段
+        textContent.text = "";                            // 清空
+
+        for(int i = 0; i < dialog.Length; i++)      // 跑對話第一個字到最後一個字
+        {
+            textContent.text += dialog[i];                // 對話內容.文字 += 對話[]
+            yield return new WaitForSeconds(printSpeed);  // 
+        }
+    }
+
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other)
