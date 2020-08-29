69499930e33d2f036ac4daceeb0cd1f6b6797f47
HWChia <66512878+xmas21@users.noreply.github.com>
2020-08-29 15:46:21 +0800

    NPC對話觸發

---
+++ 3DRPG/Assets/Scripts/NPC.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC 資料")]
+    public NPCData data;
+    [Header("對話區域")]
+    public GameObject paneldialog;
+    [Header("說話者")]
+    public Text textName;
+    [Header("說話內容")]
+    public Text textContent;
+
+    public void dialog()
+    {
+        paneldialog.SetActive(true);
+        textName.text = name;
+        textContent.text = data.dialogs[0];
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "阿兜")dialog();     
+    }
+}
---
+++ 3DRPG/Assets/Scripts/NPCData.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+// 列舉 : 下拉式選單
+
+public enum NPCState
+{
+    NoMission , Missioning , Finish
+}
+
+
+// ScriptableObject 腳本化物件 : 可儲存於專案的資料
+[CreateAssetMenu(fileName = "NPC 資料",menuName = "HWC/NPC 資料")]
+public class NPCData : ScriptableObject
+{
+    [Header("NPC 狀態")]
+    public NPCState _NPCState = NPCState.NoMission;
+    [Header("任務需求數量")]
+    public int count;
+    [Header("對話 : 未取得任務，任務進行中，任務完成")]
+    public string[] dialogs = new string[3];
+}
